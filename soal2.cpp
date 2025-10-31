dbbc9e9d96f7c59251ef54163910f9819a045132
Nasywa Zavira (Nulla427) <nasywazaviralubis@gmail.com>
2025-10-31 15:22:14 +0700

    update linked list

--- soal2.cpp
+++ soal2.cpp
@@ -1,32 +1,75 @@
 #include <iostream>
-#include <deque>
 #include <string>
 using namespace std;
 
+struct Node {
+    string nama;
+    Node* next;
+};
+
+Node* head = NULL;
+Node* tail = NULL;
+
+// Tambah pasien normal (belakang)
+void tambahNormal(string nama) {
+    Node* baru = new Node;
+    baru->nama = nama;
+    baru->next = NULL;
+
+    if (head == NULL) {       // jika antrian kosong
+        head = tail = baru;
+    } else {
+        tail->next = baru;
+        tail = baru;
+    }
+}
+
+// Tambah pasien darurat (depan)
+void tambahDarurat(string nama) {
+    Node* baru = new Node;
+    baru->nama = nama;
+    baru->next = head;
+
+    head = baru;
+    if (tail == NULL) {
+        tail = baru;
+    }
+}
+
+// Layani pasien (hapus depan)
+void layani() {
+    if (head != NULL) {
+        cout << head->nama << endl;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+
+        if (head == NULL) {
+            tail = NULL;
+        }
+    }
+}
+
 int main() {
     int N;
-    cin >> N;
+    cin >> N; //untuk banyak perintah
 
-    deque<string> antrian;
-    
     for(int i = 0; i < N; i++){
         string cmd, nama;
         cin >> cmd;
 
         if(cmd == "DATANG"){
             cin >> nama;
-            antrian.push_back(nama);   // normal: masuk belakang
+            tambahNormal(nama);
         }
         else if(cmd == "DARURAT"){
             cin >> nama;
-            antrian.push_front(nama);  // darurat: masuk depan
+            tambahDarurat(nama);
         }
         else if(cmd == "LAYANI"){
-            if(!antrian.empty()){
-                cout << antrian.front() << endl; // pasien di paling depan
-                antrian.pop_front();             // keluar setelah dilayani
-            }
+            layani();
         }
     }
+
     return 0;
 }
