4eb070a6bd0ca9c3c20b718460008bb3acedcac8
Nasywa Zavira (Nulla427) <nasywazaviralubis@gmail.com>
2025-11-04 10:42:27 +0700

    add comments

--- soal2.cpp
+++ soal2.cpp
@@ -23,6 +23,9 @@ void tambahNormal(string nama) {
         tail = baru;
     }
 }
+// Buat node baru berisi nama pasien.
+// Jika antrian kosong → head dan tail menunjuk ke node tersebut
+// Jika tidak kosong → sambungkan node baru ke tail, lalu pindahkan tail ke node baru itu.
 
 // Tambah pasien darurat (depan)
 void tambahDarurat(string nama) {
@@ -35,6 +38,10 @@ void tambahDarurat(string nama) {
         tail = baru;
     }
 }
+// Buat node baru berisi nama pasien.
+// Hubungkan node baru sehingga next-nya menunjuk ke head.
+// Geser head ke node baru tersebut.
+// Jika antrian sebelumnya kosong, maka tail juga ikut ke node ini.
 
 // Layani pasien (hapus depan)
 void layani() {
@@ -49,6 +56,11 @@ void layani() {
         }
     }
 }
+// Jika antrian tidak kosong, tampilkan nama pasien yang dilayani (cout head->nama)
+// Pindahkan head ke node berikutnya.
+// Hapus node lama.
+// Jika setelah dihapus antrian menjadi kosong => set tail = NULL.
+
 
 int main() {
     int N;
@@ -73,3 +85,9 @@ int main() {
 
     return 0;
 }
+// Program membaca jumlah perintah N
+// Setiap perintah dapat berupa:
+// DATANG nama => panggil tambahNormal(nama)
+// DARURAT nama => panggil tambahDarurat(nama)
+// LAYANI => panggil layani()
+
